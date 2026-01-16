b5e39faaa31a01b4ae01aa91818186adc435ba7b
Ricardo Montoya <montoya.ar94@gmail.com>
2026-01-15 21:24:43 -0500

    Added models for TMDB API now playing request

---
+++ Components/Models/MovieListResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieTime.Components.Models
+{
+
+    public class MovieListResponse
+    {
+        public int Page { get; set; }
+        public List<Movie> Results { get; set; } = [];
+        public int TotalPages { get; set; }
+        public int TotalResults { get; set; }
+    }
+
+}
