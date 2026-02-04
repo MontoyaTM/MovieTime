bd724404cc361959c218e7cffd7497315329cd8e
Ricardo Montoya <montoya.ar94@gmail.com>
2026-02-04 18:51:46 -0500

    Created a service layers for TMDB API requests

---
+++ Services/TMDBService.cs
@@ -0,0 +1,99 @@
+﻿using MovieTime.Components.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace MovieTime.Services
+{
+    public class TMDBService
+    {
+        private readonly HttpClient _http;
+        private readonly JsonSerializerOptions _jsonOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+        };
+
+        public TMDBService(HttpClient httpClient, IConfiguration config)
+        {
+            _http = httpClient;
+            string? tmdbKey = config["TMDB_Key"];
+
+            if(!string.IsNullOrWhiteSpace(tmdbKey))
+            {
+                _http.DefaultRequestHeaders.Authorization = new("Bearer", tmdbKey);
+            }
+        }
+
+        public async Task<MovieListResponse> GetNowPlayingMovies()
+        {
+            string url = "https://api.themoviedb.org/3/movie/now_playing?region=US&language=en-US";
+
+            MovieListResponse response = await _http.GetFromJsonAsync<MovieListResponse>(url, _jsonOptions)
+                ?? throw new HttpIOException(HttpRequestError.InvalidResponse, "Failed to retrieve now-playing movies response.");
+
+            foreach (Movie movie in response.Results)
+            {
+                if (string.IsNullOrWhiteSpace(movie.PosterPath))
+                {
+                    movie.PosterPath = "/images/Poster.png";
+                }
+                else
+                {
+                    movie.PosterPath = $"https://image.tmdb.org/t/p/w500{movie.PosterPath}";
+                }
+            }
+
+            return response;
+        }
+
+        public async Task<MovieListResponse> GetPopularMovies()
+        {
+            string url = "https://api.themoviedb.org/3/movie/popular?region=US&language=en-US";
+
+            MovieListResponse response = await _http.GetFromJsonAsync<MovieListResponse>(url, _jsonOptions)
+                ?? throw new HttpIOException(HttpRequestError.InvalidResponse, "Failed to retrieve popular movies response.");
+
+            foreach(Movie movie in response.Results)
+            {
+                if (string.IsNullOrWhiteSpace(movie.PosterPath))
+                {
+                    movie.PosterPath = "/images/Poster.png";
+                }
+                else
+                {
+                    movie.PosterPath = $"https://image.tmdb.org/t/p/w500{movie.PosterPath}";
+                }
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Search for movies
+        /// </summary>
+        /// <param name="query">User supplied query</param>
+        /// <returns></returns>
+        /// <exception cref="HttpIOException"></exception>
+        public async Task<MovieListResponse> SearchMovies(string query)
+        {
+            string url = $"https://api.themoviedb.org/3/search/movie?query={query}&include_adult=false&language=en-us";
+
+            MovieListResponse response = await _http.GetFromJsonAsync<MovieListResponse>(url, _jsonOptions)
+                ?? throw new HttpIOException(HttpRequestError.InvalidResponse, "Search results cannot be loaded.");
+
+            foreach (Movie movie in response.Results)
+            {
+                if (string.IsNullOrWhiteSpace(movie.PosterPath))
+                {
+                    movie.PosterPath = "/images/Poster.png";
+                }
+                else
+                {
+                    movie.PosterPath = $"https://image.tmdb.org/t/p/w500{movie.PosterPath}";
+                }
+            }
+
+            return response;
+        }
+
+    }
+}
