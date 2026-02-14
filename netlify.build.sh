1725830ecfd7ea44f4c915aaf4cd222b2268e289
Ricardo Montoya <montoya.ar94@gmail.com>
2026-02-13 20:11:34 -0500

    Added Netlify build files

---
+++ netlify.build.sh
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env bash
+set -e
+
+## install latest .NET 8.0 release
+pushd /tmp
+wget https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.sh
+
+chmod u+x /tmp/dotnet-install.sh
+/tmp/dotnet-install.sh --channel 8.0
+popd
+
+## publish project to known location for subsequent deployment by Netlify
+dotnet publish MovieTime.csproj -c Release -o release
