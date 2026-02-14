ca18649f24a3417abc52b1e218a924291fc20742
Ricardo Montoya <montoya.ar94@gmail.com>
2026-02-13 21:58:54 -0500

    Create the Netlify Edge Function

---
+++ netlify/edge-functions/tmdb.js
@@ -0,0 +1,19 @@
+﻿export default async (request, context) => {
+    const key = Netlify.env.get('API_KEY');
+    let tmdbUrl = Netlify.env.get('API_URL');
+
+    if (!tmdbUrl.endsWith('/')) tmdbUrl += '/';
+
+    const url = new URL(request.url);
+    const tmdbPath = url.pathname.replace('/tmdb/', '');
+
+    return await fetch(tmdbUrl + tmdbPath + url.search, {
+        headers: {
+            'Authorization': `Bearer ${key}`
+        }
+    });
+};
+
+export const config = {
+    path: '/tmdb/*'
+}
